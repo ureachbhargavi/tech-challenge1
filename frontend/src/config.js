a8bff03afd107b2f07cd0b96326cf72a9633a2e7
Sai S <ureachbhargavi@gmail.com>
2025-11-10 22:39:47 -0600

    Resolving JSON parsing error (added/ after api in backendURL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-1141213317.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-1141213317.us-east-2.elb.amazonaws.com/api/"
 };
 export default config;
